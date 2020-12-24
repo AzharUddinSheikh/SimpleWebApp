7601fa32c877c23df23d60dd5f48805ae5566126
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-24 15:02:00 +0530

    our logged in session is working fine also we have provided logout button which is not working yet but soon we ll define our logout session also we have written code to get mail from flask if someone sign up in our website not yet tested

--- MY PROJECT/webapp/main.py
+++ MY PROJECT/webapp/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, abort
+from flask import Flask, render_template, request, redirect, abort, session
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_mail import Mail, Message
+
 
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
@@ -11,6 +13,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = params['localserver']
 db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'azharsheikh760@gmail.com',
+    "MAIL_PASSWORD": 'Valar123@'
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 
 class Details(db.Model):
@@ -31,15 +46,19 @@ class Details(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if ('user' in session and session['user'] == params['adminusername']):
+        return render_template('dashboard.html', params=params)
+
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
         if (username == params['adminusername'] and password == params['adminpassword']):
-            return render_template('dashboard.html')
+            session['user'] = username
+            return render_template('dashboard.html', params=params)
 
         else:
-            abort(404)
+            abort(500)
 
     return render_template('login.html')
 
@@ -57,6 +76,11 @@ def createnew():
                         phone=phone, password=password)
         db.session.add(entry)
         db.session.commit()
+
+        msg = Message('Message from blog azhar ', sender=email,
+                      recipients=params['myemail'], body=phone)
+
+        mail.send(msg)
 
         return redirect('/')
 
